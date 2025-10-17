6a3ea9ff1070d0e6fb30041ccd1bd38275eaf3cc
jakubkromka55 <jakubkromka1tf@gmail.com>
2025-10-17 15:23:46 +0200

    Arrays

--- ArraysAndStrings/Program.cs
+++ ArraysAndStrings/Program.cs
@@ -9,6 +9,7 @@ string[] names = {
 };
 
 
+
 // Print all array elements, *perLine* elements per one line
 // After all elements except last one should be ", " - also on the end of lines.
 // After last element should be ".".
@@ -77,20 +78,85 @@ void PrintColumns(string[] t, int perLine, int width)
 Console.WriteLine("\nPrintGroups(names, 3):\n");
 PrintGroups(names, 3);
 
+/*
+Mickey Mouse, Minnie Mouse, Donald Duck,
+Goofy, Pluto, Daisy Duck,
+Simba, Nala, Timon,
+Pumbaa, Mufasa, Ariel,
+Flounder, Sebastian, Ursula,
+Belle, Beast, Gaston,
+Cinderella, Prince Charming, Aurora,
+Maleficent, Rapunzel, Flynn Rider,
+Elsa, Anna, Olaf,
+Moana, Maui, Hercules.
+*/
+
 Console.WriteLine("\nPrintGroups(names, 5):\n");
 PrintGroups(names, 5);
+
+/*
+Mickey Mouse, Minnie Mouse, Donald Duck, Goofy, Pluto,
+Daisy Duck, Simba, Nala, Timon, Pumbaa,
+Mufasa, Ariel, Flounder, Sebastian, Ursula,
+Belle, Beast, Gaston, Cinderella, Prince Charming,
+Aurora, Maleficent, Rapunzel, Flynn Rider, Elsa,
+Anna, Olaf, Moana, Maui, Hercules. 
+*/
 
 Console.WriteLine("\nPrintGroups(names, 7):\n");
 PrintGroups(names, 7);
 
+/*
+Mickey Mouse, Minnie Mouse, Donald Duck, Goofy, Pluto, Daisy Duck, Simba,
+Nala, Timon, Pumbaa, Mufasa, Ariel, Flounder, Sebastian,
+Ursula, Belle, Beast, Gaston, Cinderella, Prince Charming, Aurora,
+Maleficent, Rapunzel, Flynn Rider, Elsa, Anna, Olaf, Moana,
+Maui, Hercules.
+*/
+
+// For width = 40 don't worry if result will be wrapped due to screen width.
+
 Console.WriteLine("\nPrintGroups(names, 40):\n");
 PrintGroups(names, 40);
+
+/*
+Mickey Mouse, Minnie Mouse, Donald Duck, Goofy, Pluto, Daisy Duck, Simba, Nala, Timon, Pumbaa, Mufasa, Ariel, Flounder,
+Sebastian, Ursula, Belle, Beast, Gaston, Cinderella, Prince Charming, Aurora, Maleficent, Rapunzel, Flynn Rider, Elsa, A
+nna, Olaf, Moana, Maui, Hercules.
+*/
 
 Console.WriteLine("\n\nPrintColumns(names, 4, 17):\n");
 PrintColumns(names, 4, 17);
 
+/*
+Mickey Mouse     | Minnie Mouse     | Donald Duck      | Goofy
+Pluto            | Daisy Duck       | Simba            | Nala
+Timon            | Pumbaa           | Mufasa           | Ariel
+Flounder         | Sebastian        | Ursula           | Belle
+Beast            | Gaston           | Cinderella       | Prince Charming
+Aurora           | Maleficent       | Rapunzel         | Flynn Rider
+Elsa             | Anna             | Olaf             | Moana
+Maui             | Hercules         |
+*/
+
 Console.WriteLine("\n\nPrintColumns(names, 5, 15):\n");
 PrintColumns(names, 5, 15);
 
+/*
+Mickey Mouse   | Minnie Mouse   | Donald Duck    | Goofy          | Pluto
+Daisy Duck     | Simba          | Nala           | Timon          | Pumbaa
+Mufasa         | Ariel          | Flounder       | Sebastian      | Ursula
+Belle          | Beast          | Gaston         | Cinderella     | Prince Charming
+Aurora         | Maleficent     | Rapunzel       | Flynn Rider    | Elsa
+Anna           | Olaf           | Moana          | Maui           | Hercules
+*/
+
 Console.WriteLine("\n\nPrintColumns(names, 8, 10):\n");
 PrintColumns(names, 8, 10);
+
+/*
+Mickey Mou| Minnie Mou| Donald Duc| Goofy     | Pluto     | Daisy Duck| Simba     | Nala
+Timon     | Pumbaa    | Mufasa    | Ariel     | Flounder  | Sebastian | Ursula    | Belle
+Beast     | Gaston    | Cinderella| Prince Cha| Aurora    | Maleficent| Rapunzel  | Flynn Ride
+Elsa      | Anna      | Olaf      | Moana     | Maui      | Hercules  |
+*/
